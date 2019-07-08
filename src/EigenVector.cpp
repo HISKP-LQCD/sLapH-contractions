f5c68e64379e0d026443f0c4bd065b7d45127d9c
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-07-09 00:04:48 +0200

    make VdV test more stringent

--- src/EigenVector.cpp
+++ src/EigenVector.cpp
@@ -39,8 +39,8 @@ bool EigenVector::test_trace_sum(const ssize_t t, const bool do_throw) {
   Eigen::MatrixXcd VdV = V[t].adjoint() * V[t];
   const std::complex<double> trace = VdV.trace();
   const std::complex<double> sum = VdV.sum();
-  // we allow for some deviation per eigenvector, but not much!
-  if( abs( trace.real() - V[t].cols() ) > 2*V[t].cols()*DBL_EPSILON ){
+  // we allow for some deviation
+  if( abs( trace.real() - V[t].cols() ) > 2*DBL_EPSILON ){
     fail = true;
     std::stringstream message;
     // when printing the error, make sure to print exactly what is above in the if statement
@@ -52,7 +52,7 @@ bool EigenVector::test_trace_sum(const ssize_t t, const bool do_throw) {
       std::cout << message.str() << std::endl;
     }
   }
-  if( abs( sum.real() - V[t].cols() ) > 2*V[t].cols()*DBL_EPSILON ){
+  if( abs( sum.real() - V[t].cols() ) > 2*DBL_EPSILON ){
     fail = true;
     std::stringstream message;
     message << "Sum of VdaggerV elements: " << sum << " with real part: " << 
