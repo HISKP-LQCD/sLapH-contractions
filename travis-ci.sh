f1ead186981ee522da5dd295c3205c632530bbf8
pittlerf <pittler@hiskp.uni-bonn.de>
2019-08-06 00:15:16 +0200

    Try to fix the integration test once mor

--- travis-ci.sh
+++ travis-ci.sh
@@ -23,6 +23,7 @@ ubuntu_packages=(
     libboost-filesystem-dev libboost-system-dev libboost-program-options-dev
     libgtest-dev
 )
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 762E3157
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 #sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
