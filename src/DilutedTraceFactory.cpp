ba76c773ce3707ceebb87f6b9cb44e085482746a
Martin Ueding <dev@martin-ueding.de>
2019-10-22 15:55:35 +0200

    Parallelize DilutedTraceFactory

--- src/DilutedTraceFactory.cpp
+++ src/DilutedTraceFactory.cpp
@@ -17,9 +17,12 @@ void DilutedTrace1Factory<DilutedFactorType::Q1>::build(Key const &time_key) {
   auto t = time_key[0];
   auto b = dilution_scheme.time_to_block(t);
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t}][i] = factor_to_trace(df[{t, b}].at({c_look[0]}));
+    auto const &value = factor_to_trace(df[{t, b}].at({c_look[0]}));
+#pragma omp critical(DilutedTraceFactory_02_build)
+    Tr[{t}][i] = value;
   }
 }
 
@@ -35,10 +38,13 @@ void DilutedTrace2Factory<DilutedFactorType::Q1, DilutedFactorType::Q1>::build(
   auto b1 = dilution_scheme.time_to_block(t1);
   auto b2 = dilution_scheme.time_to_block(t2);
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t1, t2}][i] =
+    auto const &value =
         factor_to_trace(df1[{t1, b2}].at({c_look[0]}), df2[{t2, b1}].at({c_look[1]}));
+#pragma omp critical(DilutedTraceFactory_02_build)
+    Tr[{t1, t2}][i] = value;
   }
 }
 
@@ -53,10 +59,13 @@ void DilutedTrace2Factory<DilutedFactorType::Q0, DilutedFactorType::Q2>::build(
   auto t2 = time_key[1];
   auto b2 = dilution_scheme.time_to_block(t2);
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
-    const auto &c_look = diagram_index_collection[i];
-    Tr[{t1, t2}][i] =
+    auto const &c_look = diagram_index_collection[i];
+    auto const &value =
         factor_to_trace(df1[{t2}].at({c_look[1]}), df2[{b2, t1, b2}].at({c_look[0]}));
+#pragma omp critical(DilutedTraceFactory_02_build)
+    Tr[{t1, t2}][i] = value;
   }
 }
 
@@ -81,10 +90,13 @@ void DilutedTrace3Factory<DilutedFactorType::Q1,
     multiply<1, 1>(L1, {c_look[0], c_look[1]}, df1[{t1, b2}], df2[{t2, b3}]);
   }
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t1, t2, t3}][i] =
+    auto const &value =
         factor_to_trace(L1[{c_look[0], c_look[1]}], df3[{t3, b1}].at({c_look[2]}));
+#pragma omp critical(DilutedTraceFactory_111_build)
+    Tr[{t1, t2, t3}][i] = value;
   }
 }
 
@@ -111,10 +123,13 @@ void DilutedTrace3Factory<DilutedFactorType::Q1,
     multiply<1, 1>(L1, {c_look[2], c_look[0]}, df2[{t1}], df3[{b1, t1, b2}]);
   }
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t1, t2, t3}][i] =
+    auto const &value =
         factor_to_trace(L1[{c_look[2], c_look[0]}], df1[{t2, b3}].at({c_look[1]}));
+#pragma omp critical(DilutedTraceFactory_102_build)
+    Tr[{t1, t2, t3}][i] = value;
   }
 }
 
@@ -146,10 +161,13 @@ void DilutedTrace4Factory<DilutedFactorType::Q1,
     multiply<1, 1>(L2, {c_look[2], c_look[3]}, df3[{t2, b3}], df4[{t3, b0}]);
   }
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t0, t1, t2, t3}][i] =
+    auto const &value =
         factor_to_trace(L1[{c_look[0], c_look[1]}], L2[{c_look[2], c_look[3]}]);
+#pragma omp critical(DilutedTraceFactory_1111_build)
+    Tr[{t0, t1, t2, t3}][i] = value;
   }
 }
 
@@ -174,13 +192,16 @@ void DilutedTrace4Factory<DilutedFactorType::Q2,
   auto const b2 = dilution_scheme.time_to_block(t2);
   // auto const b3 = dilution_scheme.time_to_block(t3);
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
 
     auto const &l1 = dpf_.get({b0, t1, b2, t2}, {c_look[1], c_look[2]});
     auto const &l2 = dpf_.get({b2, t3, b0, t0}, {c_look[3], c_look[0]});
+    auto const &value = factor_to_trace(l1, l2);
 
-    Tr[time_key][i] = factor_to_trace(l1, l2);
+#pragma omp critical(DilutedTraceFactory_2020_build)
+    Tr[time_key][i] = value;
   }
 
 #ifdef SLAPH_CLEAR_QQ_CACHE
@@ -215,6 +236,7 @@ void DilutedTrace6Factory<DilutedFactorType::Q2,
   auto const b4 = dilution_scheme.time_to_block(t4);
   // auto const b5 = dilution_scheme.time_to_block(t5);
 
+#pragma omp parallel for
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     auto const &c_look = diagram_index_collection[i];
 
@@ -225,8 +247,9 @@ void DilutedTrace6Factory<DilutedFactorType::Q2,
     assert(l01.size() > 0);
     assert(l23.size() > 0);
     assert(l45.size() > 0);
-
-    Tr[time_key][i] = factor_to_trace(l01 * l23, l45);
+    auto const &value = factor_to_trace(l01 * l23, l45);
+#pragma omp critical(DilutedTraceFactory_202020_build)
+    Tr[time_key][i] = value;
   }
 
 #ifdef SLAPH_CLEAR_QQ_CACHE
