e0e16abc20616e3f7ed3dbf9208dc6d7c2b79271
Martin Ueding <dev@martin-ueding.de>
2019-11-04 10:40:05 +0100

    Move some functions to the source files

--- include/DilutedProductFactory.hpp
+++ include/DilutedProductFactory.hpp
@@ -14,30 +14,9 @@ class DilutedProductFactoryQ0Q2 {
                             DilutedFactorFactory<DilutedFactorType::Q2> &factory_q2)
       : factory_q0_(factory_q0), factory_q2_(factory_q2) {}
 
-  void request(TimeKey const &time_key, QnKey const &qn_key) {
-    requests_.insert(std::make_pair(time_key, qn_key));
-  }
+  void request(TimeKey const &time_key, QnKey const &qn_key);
 
-  void build_all() {
-    TimingScope<3> timing_scope("DilutedProductFactoryQ0Q2::build_all");
-
-    std::vector<FullKey> unique_requests;
-    unique_requests.reserve(requests_.size());
-    for (auto const &request : requests_) {
-      if (Q0Q2_.count(request.first) == 0 ||
-          Q0Q2_.at(request.first).count(request.second) == 0) {
-        unique_requests.push_back(request);
-        Q0Q2_[request.first];
-      }
-    }
-
-    for (int i = 0; i < ssize(unique_requests); ++i) {
-      auto const &request = unique_requests[i];
-      build(request.first, request.second);
-    }
-
-    requests_.clear();
-  }
+  void DilutedProductFactoryQ0Q2::build_all();
 
   std::vector<DilutedFactor> const &get(TimeKey const &time_key, QnKey const &key) {
     return Q0Q2_.at(time_key).at(key);
--- src/DilutedProductFactory.cpp
+++ src/DilutedProductFactory.cpp
@@ -2,9 +2,8 @@
 
 #include "timings.hpp"
 
-void DilutedProductFactoryQ0Q2::build(TimeKey const &time_key,
-                                      QnKey const &key) {
-  TimingScope<5> timing_scope("DilutedProductFactoryQ0Q2::build");
+void DilutedProductFactoryQ0Q2::request(TimeKey const &time_key, QnKey const &qn_key) {
+  TimingScope<5> timing_scope("DilutedProductFactoryQ0Q2::request");
 
   // Extract time keys.
   int constexpr nt1 = 3;
@@ -16,8 +15,42 @@ void DilutedProductFactoryQ0Q2::build(TimeKey const &time_key,
 
   factory_q2_.request(time_key1);
   factory_q0_.request(time_key2);
-  factory_q2_.build_all();
-  factory_q0_.build_all();
+
+  requests_.insert(std::make_pair(time_key, qn_key));
+}
+
+void DilutedProductFactoryQ0Q2::build_all() {
+  TimingScope<3> timing_scope("DilutedProductFactoryQ0Q2::build_all");
+
+  std::vector<FullKey> unique_requests;
+  unique_requests.reserve(requests_.size());
+  for (auto const &request : requests_) {
+    if (Q0Q2_.count(request.first) == 0 ||
+        Q0Q2_.at(request.first).count(request.second) == 0) {
+      unique_requests.push_back(request);
+      Q0Q2_[request.first][request.second];
+    }
+  }
+
+  //#pragma omp parallel for
+  for (int i = 0; i < ssize(unique_requests); ++i) {
+    auto const &request = unique_requests[i];
+    build(request.first, request.second);
+  }
+
+  requests_.clear();
+}
+
+void DilutedProductFactoryQ0Q2::build(TimeKey const &time_key, QnKey const &key) {
+  TimingScope<5> timing_scope("DilutedProductFactoryQ0Q2::build");
+
+  // Extract time keys.
+  int constexpr nt1 = 3;
+  int constexpr nt2 = 1;
+  std::array<int, nt1> time_key1;
+  std::array<int, nt2> time_key2;
+  std::copy_n(std::begin(time_key) + 0, nt1, std::begin(time_key1));
+  std::copy_n(std::begin(time_key) + nt1, nt2, std::begin(time_key2));
 
   multiply<1, 1>(Q0Q2_[time_key], key, factory_q2_[time_key1], factory_q0_[time_key2]);
 }
