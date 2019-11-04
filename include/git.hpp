254fa136081ea6f2b17d4b57ffdae0eae2a9f340
Martin Ueding <dev@martin-ueding.de>
2019-11-04 14:31:45 +0100

    Move git info to a source file for faster incremental building

---
+++ include/git.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+extern char const *const git_sha1;
+extern char const *const git_refspec;
+extern char const *const git_changes;
+extern char const *const git_user;
+extern char const *const git_host;
